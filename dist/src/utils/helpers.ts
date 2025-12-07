6ffb52c4109eb93bd39cd2d19c374914786bd057
Beemil <valeskavr17@gmail.com>
2025-12-07 16:23:28 -0600

    Create utils/ Helpers.ts

---
+++ dist/src/utils/helpers.ts
@@ -0,0 +1,11 @@
+export function cryptoRandomId() {
+  return Math.random().toString(36).slice(2, 10);
+}
+
+export function escapeHtml(s: string) {
+  return s
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;");
+}
